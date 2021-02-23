f24362864726a881a40b7db58316513107d6763b
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-22 18:43:15 -0600

    added components and API call to Google Books

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import Search from './pages/Search';
+// import Saved from './pages/Saved';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Search />
+          </Route>
+          {/* <Route exact path="/saved">
+            <Saved />
+          </Route> */}
+        </Switch>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </BrowserRouter>
   );
 }
 
-
 export default App;
+
---
+++ client/src/pages/Search.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, FormBtn } from "../components/Form";
+
+function Books() {
+    const [books, setBooks] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
+
+    // useEffect(() => {
+    //     searchBooks()
+    // }, [])
+
+    // // Use for API call
+    // function searchGoogle() {
+    //     API.getBooks()
+    //         .then(res => {
+    //             console.log('res.data :>> ', res.data);
+
+    //         })
+    //         .catch(err => console.log(err));
+    // };
+
+    function handleInputChange(event) {
+        const { value } = event.target;
+        setSearchTerm(value)
+    };
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (searchTerm.length) {
+            API.searchBooks(searchTerm)
+                .then(res => {
+                    setBooks(res.data.items)
+                    console.log('res :>> ', res);
+                })
+                .catch(err => console.log(err));
+        }
+    };
+
+    return (
+        <Container fluid>
+            <Row>
+                <Col size="md-6">
+                    <Jumbotron>
+                        <h1>What Books Should I Read?</h1>
+                    </Jumbotron>
+                    <form>
+                        <Input
+                            onChange={handleInputChange}
+                            name="search"
+                            placeholder="Search"
+                        />
+                        <FormBtn
+                            disabled={!searchTerm.length}
+                            onClick={handleFormSubmit}
+                        >
+                            Search
+              </FormBtn>
+                    </form>
+                </Col>
+                <Col size="md-6 sm-12">
+                    <Jumbotron>
+                        <h1>Books On My List</h1>
+                    </Jumbotron>
+                    {books.length ? (
+                        <List>
+                            {books.map(book => (
+                                <ListItem key={book._id}>
+                                    <Link to={"/books/" + book._id}>
+                                        <strong>
+                                            {book.title} by {book.author}
+                                        </strong>
+                                    </Link>
+                                </ListItem>
+                            ))}
+                        </List>
+                    ) : (
+                            <h3>No Results to Display</h3>
+                        )}
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+
+export default Books;
