00d86b4927a7b396892a48fa84ab53b15aac6e50
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-24 08:36:26 -0600

    updated API logic

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,23 +1,20 @@
 import axios from "axios";
 
 export default {
-    // Gets all books
+    // Searches Google Books API
+    searchBooks: function (search) {
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + search)
+    },
+    // Gets all books from database
     getBooks: function () {
         return axios.get("/api/books");
     },
-    // Gets the book with the given id
-    getBook: function (id) {
-        return axios.get("/api/books/" + id);
-    },
-    // Deletes the book with the given id
+    // Deletes the book with the given id from the database
     deleteBook: function (id) {
         return axios.delete("/api/books/" + id);
     },
     // Saves a book to the database
     saveBook: function (bookData) {
         return axios.post("/api/books", bookData);
-    },
-    searchBooks: function (search) {
-        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + search)
     }
 };
