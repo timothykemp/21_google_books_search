79bd60c1806c9500233c5b06163325d9ba565576
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-24 08:47:14 -0600

    updated Nav component to include links

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        Google Books
-      </a>
+      <Link to="/" className="navbar-brand" >Google Books</Link>
+      <Link to="/" className="nav-link" style={{ color: "white" }} >Search</Link>
+      <Link to="/saved" className="nav-link" style={{ color: "white" }}  >Saved</Link>
     </nav>
   );
 }
