38f1374787fe5c5bee84bc27a2c5d1a6e505ff12
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-24 09:03:39 -0600

    updated Jumbotron component

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Jumbotron({ children }) {
+function Jumbotron() {
   return (
     <div
       style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
       className="jumbotron"
     >
-      {children}
+      <h1>(React) Google Books Search</h1>
+      <h4>Search for and save books of interest!</h4>
     </div>
   );
 }
