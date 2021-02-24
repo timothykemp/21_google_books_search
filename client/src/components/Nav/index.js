f553c4dac20d3fc93b77ae695971e08d7108c4c6
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-24 08:53:45 -0600

    updated styling of navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <Link to="/" className="navbar-brand" >Google Books</Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary" style={{ justifyContent: "left" }}>
+      <Link to="/" className="navbar-brand" style={{ fontSize: "1.5rem", paddingTop: 0 }} >Google Books</Link>
       <Link to="/" className="nav-link" style={{ color: "white" }} >Search</Link>
       <Link to="/saved" className="nav-link" style={{ color: "white" }}  >Saved</Link>
     </nav>
